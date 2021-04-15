7a450bae5e1c51bd3b32b9747fb410bf2482639c
seriouscat96 <thisisbusiness@yandex.ru>
2021-04-15 22:25:50 +0300

    fixed helmet

--- src/middlewares/helmet.js
+++ src/middlewares/helmet.js
@@ -1,5 +1,11 @@
 const helmet = require('helmet');
 
 module.exports = helmet({
-  frameguard: { action: 'deny' },
+  frameguard: { action: 'sameorigin' },
+  referrerPolicy: { policy: 'strict-origin' },
+  hsts: {
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true,
+  },
 });
