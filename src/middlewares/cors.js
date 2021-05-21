2a26fdf74666e4b401f5b1b80593adee947dbe3c
seriouscat96 <thisisbusiness@yandex.ru>
2021-05-21 16:46:04 +0300

    fixed cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const options = {
   origin: [
     'http://localhost:3000',
-    'http://movies-explorer.jumpingcrab.com',
-    'https://movies-explorer.jumpingcrab.com',
+    /movies-explorer\.jumpingcrab\.com$/,
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
